f80acc1e8ae59d166061b416d5719a2c5ce85af9
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-14 08:05:20 +0900

    Update RUBY.rb

--- RUBY.rb
+++ RUBY.rb
@@ -18,3 +18,16 @@ long_time_re = Regexp.new('^a*b?a*()\1$', timeout: Float::INFINITY)
 long_time_re =~ "a" * 50000 + "x" 
 
 foo1::BAR1, foo2::BAR2 = baz1, baz2
+
+def target(**kw)
+  end
+
+  ruby2_keywords def bar(*args)
+    target(*args)
+  end
+
+  ruby2_keywords def foo(*args)
+    bar(*args)
+  end
+
+  foo(k: 1)
