a1e89e027c059020f0996832037ee60eee90d02a
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-14 08:22:15 +0900

    Create PHP.php

---
+++ PHP.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?> 
+  <?php phpinfo(); ?>
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+$txt = "Hello";
+?>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+echo strlen("Hello World!");
+?>
+//
+/* comments */
+  
+  
+</body>
+</html>
